3ddb8d9aca2900b9ff5c545013a9fc0b23fbf5bd
SerenityZenDev <tjrqo2000@gmail.com>
2024-11-08 10:00:30 +0900

    feat: 프로젝트 시작

---
+++ src/main/java/org/example/javajwtonboading/JavaJwtOnboadingApplication.java
@@ -0,0 +1,13 @@
+package org.example.javajwtonboading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJwtOnboadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJwtOnboadingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/javajwtonboading/JavaJwtOnboadingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javajwtonboading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJwtOnboadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
