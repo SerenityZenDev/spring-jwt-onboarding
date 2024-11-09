2e87d6b54f16b7d69fbc0eaa95fed68f10ce8858
SerenityZenDev <tjrqo2000@gmail.com>
2024-11-09 10:04:51 +0900

    feat: 입력값 Valid 설정

--- src/main/java/org/example/javajwtonboading/presentation/request/SignupRequestDTO.java
+++ src/main/java/org/example/javajwtonboading/presentation/request/SignupRequestDTO.java
@@ -1,8 +1,19 @@
 package org.example.javajwtonboading.presentation.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record SignupRequestDTO(
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     String username,
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     String password,
+
+    @NotBlank(message = "Nickname cannot be blank")
+    @Size(min = 3, max = 20, message = "Nickname must be between 3 and 20 characters")
     String nickname
 ) {
 
