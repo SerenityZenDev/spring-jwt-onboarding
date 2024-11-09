3eeff79251ce22fdc37a3ea35dc78c9f0ca69f0b
SerenityZenDev <tjrqo2000@gmail.com>
2024-11-09 18:27:47 +0900

    chore: 코드 컨벤션

--- src/test/java/org/example/javajwtonboading/jwt/JwtUtilTest.java
+++ src/test/java/org/example/javajwtonboading/jwt/JwtUtilTest.java
@@ -1,18 +1,17 @@
 package org.example.javajwtonboading.jwt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import io.jsonwebtoken.Claims;
-import org.example.javajwtonboading.infrastructure.config.jwt.JwtUtil;
+import java.util.Date;
 import org.example.javajwtonboading.domain.model.UserRole;
+import org.example.javajwtonboading.infrastructure.config.jwt.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.util.Date;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -48,7 +47,8 @@ class JwtUtilTest {
 
     @Test
     void 만료된_토큰_검증_테스트() {
-        String expiredToken = jwtUtil.createToken(1L, "testUser", UserRole.ROLE_USER, new Date(System.currentTimeMillis() - 1000));
+        String expiredToken = jwtUtil.createToken(1L, "testUser", UserRole.ROLE_USER,
+            new Date(System.currentTimeMillis() - 1000));
         assertFalse(jwtUtil.validateToken(expiredToken));
     }
 
