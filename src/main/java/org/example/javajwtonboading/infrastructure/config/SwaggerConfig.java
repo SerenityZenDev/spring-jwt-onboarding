1ba3642af8661991346bce395ad9f0ec47a7c9f8
SerenityZenDev <tjrqo2000@gmail.com>
2024-11-10 10:59:05 +0900

    chore: 코드컨벤션

--- src/main/java/org/example/javajwtonboading/infrastructure/config/SwaggerConfig.java
+++ src/main/java/org/example/javajwtonboading/infrastructure/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package org.example.javajwtonboading.infrastructure.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
